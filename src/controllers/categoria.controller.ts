f118a6beb895f828ffa08b3fd7059c341d0a97b6
Diaz Carlos Agustin <carlos.agustin.diaz1998@gmail.com>
2021-01-16 00:55:36 -0300

    terminando el controlador de categorias

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -70,3 +70,75 @@ export async function crearCategoria(req: Request, res: Response): Promise<void>
         })
     }
 }
+/**
+ * *Obtengo una categoria
+ * @param req 
+ * @param res 
+ */
+export async function getCategoria(req: Request, res: Response): Promise<void> {
+    try {
+        let id = await req.params.id
+        await getRepository(categoria)
+            .find({ where: { id: id } })
+            .then(result => {
+                res.json({ result: result })
+            })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(404).json({ error })
+    }
+}
+
+/**
+ * *Actualizar una categoria
+ * @param req 
+ * @param res 
+ */
+export async function updateCategoria(req: Request, res: Response): Promise<void> {
+
+    try {
+        let id = await req.params.id
+
+        await getRepository(categoria)
+            .createQueryBuilder("categoria")
+            .update(req.body)
+            .where("id = :id", { id: id })
+            .execute()
+            .then(result => {
+                if (result.raw.affectedRows > 0 && result.raw.warningCount === 0) {
+                    res.json({ "status": "OK", "msg": "Los cambios se guardaron correctamente" })
+                }
+                else {
+                    res.json({ "status": "FAIL", "msg": "No se pudieron guardar los cambios correctamente, intent nuevamente." })
+                }
+            })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(404).json({ error })
+    }
+}
+
+export async function deleteCategoria(req: Request, res: Response): Promise<void> {
+    try {
+        let id = await req.params.id
+
+        await getRepository(categoria)
+            .createQueryBuilder("categoria")
+            .delete()
+            .where("id = :id", { id: id })
+            .execute()
+            .then(result => {
+                if (result.raw.affectedRows > 0 && result.raw.warningCount === 0) {
+                    res.json({ "status": "OK", "msg": "Los cambios se guardaron correctamente" })
+                }
+                else {
+                    res.json({ "status": "FAIL", "msg": "No se pudieron guardar los cambios correctamente, intent nuevamente." })
+                }
+            })
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({ error })
+    }
+}
